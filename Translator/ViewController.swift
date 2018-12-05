63b3048335a5fe2957c20810f63873558e1291f4
Евгений <gnack@macbook-air-maria.local>
2018-12-05 18:55:19 +0500

    Initial Commit

---
+++ Translator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Translator
+//
+//  Created by Евгений on 05/12/2018.
+//  Copyright © 2018 Evgenii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
