ad8dd670f61377ad5b6c0e114bf0abc4f415f128
Евгений <gnack@macbook-air-maria.local>
2018-12-09 16:26:51 +0500

    Proof of concept

--- Translator/Message.swift
+++ Translator/Message.swift
@@ -8,13 +8,39 @@
 
 import Foundation
 
-struct Message: Codable {
-    var text: String
-    var language: String
+struct Message {
+    var text: String?
+    var language: String?
     var date: Date
     
-    enum CodingKeys: String, CodingKey {
-        case text
-        case language = "lang"
+    
+    
+    init(text: String?, language: String?, date: Date){
+        self.text = text
+        self.language = language
+        self.date = date
     }
+}
+
+extension Message: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case language = "lang"
+        case text
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        //let text: String? = try? container.decode(String.self, forKey: .text)
+        let language: String? = try? container.decode(String.self, forKey: .language)
+        let date = Date.init()
+        
+        var textContainer = try container.nestedUnkeyedContainer(forKey: .text)
+        let text = try? textContainer.decode(String.self)
+        self.init(text: text, language: language, date: date)
+    }
+}
+struct TranslationResponse {
+    var Translation: [Message]
+    
+    
 }
--- Translator/ViewController.swift
+++ Translator/ViewController.swift
@@ -8,17 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    
+    
+    var messages: [Message] = []
+    
+    let defaultSession = URLSession(configuration: .default)
+    var dataTask: URLSessionDataTask?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
-
-
+    
+    
     @IBOutlet weak var table: UITableView!
     
+    override func viewDidLayoutSubviews() {
+        table.delegate=self
+        table.dataSource=self
+        
+        var components = URLComponents.init(string: "https://translate.yandex.net/api/v1.5/tr.json/translate")
+        
+        let key = "trnsl.1.1.20181206T055317Z.85da48f152017968.c654d68a5e6f9d84366a3b6c00f92f2c5ef838bc"
+        
+        let text = "queue"
+        
+        let lang = "en-ru"
+        
+        components?.query="key=\(key)&text=\(text)&lang=\(lang)"
+        if let url = components?.url {
+            defaultSession.dataTask(with: url) { data, response, error in
+                //if let json = try? JSONSerialization.jsonObject(with: data!, options: []) {
+                DispatchQueue.main.async {
+                    if let data = data, let message=try? JSONDecoder().decode(Message.self, from: data) {
+                        self.messages.append(message)
+                    }
+                    self.table.reloadData()
+                }
+                
+                }.resume()
+        }
+    }
     
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Out", for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].text
+            
+        return cell
+    }
 }
 
 
