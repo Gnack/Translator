bb48e7d261eb46c5707f40f545d3a0b6091faa1f
Евгений <gnack@macbook-air-maria.local>
2018-12-10 16:06:16 +0500

    Send button, table view from bottom to top

--- Translator/ViewController.swift
+++ Translator/ViewController.swift
@@ -18,10 +18,14 @@ class ViewController: UIViewController, UITableViewDelegate {
     let defaultSession = URLSession(configuration: .default)
     var dataTask: URLSessionDataTask?
     
+
+    @IBOutlet weak var inputField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        table.transform = CGAffineTransform(scaleX: 1, y: -1)
         
         
     }
@@ -29,31 +33,36 @@ class ViewController: UIViewController, UITableViewDelegate {
     
     @IBOutlet weak var table: UITableView!
     
-    override func viewDidLayoutSubviews() {
-        table.delegate=self
-        table.dataSource=self
-        
-        var components = URLComponents.init(string: "https://translate.yandex.net/api/v1.5/tr.json/translate")
-        
+    @IBAction func sendButtonPress(_ sender: UIButton) {
         let key = "trnsl.1.1.20181206T055317Z.85da48f152017968.c654d68a5e6f9d84366a3b6c00f92f2c5ef838bc"
-        
-        let text = "queue"
         
         let lang = "en-ru"
         
-        components?.query="key=\(key)&text=\(text)&lang=\(lang)"
-        if let url = components?.url {
-            defaultSession.dataTask(with: url) { data, response, error in
-                //if let json = try? JSONSerialization.jsonObject(with: data!, options: []) {
-                DispatchQueue.main.async {
-                    if let data = data, let message=try? JSONDecoder().decode(TranslationResponse.self, from: data) {
-                        self.messages.append(message)
+        var components = URLComponents.init(string: "https://translate.yandex.net/api/v1.5/tr.json/translate")
+        
+        if let text = inputField?.text {
+            components?.query="key=\(key)&text=\(text)&lang=\(lang)"
+            if let url = components?.url {
+                defaultSession.dataTask(with: url) { data, response, error in
+                    //if let json = try? JSONSerialization.jsonObject(with: data!, options: []) {
+                    DispatchQueue.main.async {
+                        if let data = data, let message=try? JSONDecoder().decode(TranslationResponse.self, from: data) {
+                            self.messages.insert(message, at: 0)
+                        }
+                        self.table.reloadData()
                     }
-                    self.table.reloadData()
-                }
-                
-                }.resume()
+                    
+                    }.resume()
+            }
         }
+       
+        
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        table.delegate=self
+        table.dataSource=self
     }
     
 }
@@ -65,7 +74,8 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Out", for: indexPath)
         cell.textLabel?.text = messages[indexPath.row].translatedText
-            
+        
+        cell.contentView.transform = CGAffineTransform(scaleX: 1, y: -1)
         return cell
     }
 }
