cafd198a40dbe5f880fd73deeefc1346e4e4925d
Евгений <gnack@macbook-air-maria.local>
2018-12-11 22:21:08 +0500

    Custom cell design added, assets for buttons inserted

---
+++ Translator/TranlsatorCollectionViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  TransatorTableViewCell.swift
+//  Translator
+//
+//  Created by Евгений on 11/12/2018.
+//  Copyright © 2018 Evgenii. All rights reserved.
+//
+
+import UIKit
+
+class TranslatorCollectionViewCell: UICollectionViewCell {
+    
+    let translatedText: UITextView = UITextView()
+    
+    let originalText: UITextView = UITextView()
+    
+    let bubble: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 15
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        translatedText.font = UIFont.boldSystemFont(ofSize: 18)
+        translatedText.textColor = UIColor.white
+        originalText.font = UIFont.systemFont(ofSize: 14)
+        originalText.textColor = UIColor.white
+        
+        contentView.addSubview(bubble)
+        contentView.addSubview(originalText)
+        contentView.addSubview(translatedText)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Translator/ViewController.swift
+++ Translator/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController, UITableViewDelegate {
+class ViewController: UIViewController, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var swap: UIButton!
     
+    @IBAction func clearInput(_ sender: UIButton) {
+        inputField.text = nil
+    }
     
     var translations: [Translation] = []
     
@@ -31,26 +36,29 @@ class ViewController: UIViewController, UITableViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        table.delegate=self
-        table.dataSource=self
-        table.transform = CGAffineTransform(scaleX: 1, y: -1)
+        chatView.delegate=self
+        chatView.dataSource=self
+        chatView.transform = CGAffineTransform(scaleX: 1, y: -1)
         
-        table.register(TranslatorTableViewCell.self, forCellReuseIdentifier: "custom")
+        chatView.register(TranslatorCollectionViewCell.self, forCellWithReuseIdentifier: "custom")
         
         loadData()
     }
     
     
-    @IBOutlet weak var table: UITableView!
-   
+    
+    @IBOutlet weak var chatView: UICollectionView!
+    
+    @IBAction func swapLanguage(_ sender: UIButton) {
+        
+    }
+    
     @IBAction func sendButtonPress(_ sender: UIButton) {
         let key = "trnsl.1.1.20181206T055317Z.85da48f152017968.c654d68a5e6f9d84366a3b6c00f92f2c5ef838bc"
-        
         var components = URLComponents.init(string: "https://translate.yandex.net/api/v1.5/tr.json/translate")
         
         if let text = inputField?.text {
-            
-            
+            inputField.text = nil
             components?.query="key=\(key)&text=\(text)&lang=\(queryLang)"
             
             if let url = components?.url {
@@ -90,28 +98,79 @@ class ViewController: UIViewController, UITableViewDelegate {
             let fetchRequest = NSFetchRequest<Translation>(entityName: "Translation")
             fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
             translations = try! context.fetch(fetchRequest)
-            table.reloadData()
+            chatView.reloadData()
         }
     }
 }
 
-extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension ViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return translations.count
     }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "custom", for: indexPath)
-        if let customCell = cell as? TranslatorTableViewCell {
-            customCell.originalText?.text = translations[indexPath.row].originalText
-            customCell.translatedText?.text = translations[indexPath.row].translatedText
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "custom", for: indexPath)
+        if let customCell = cell as? TranslatorCollectionViewCell {
             
             customCell.contentView.transform = CGAffineTransform(scaleX: 1, y: -1)
+            
+            if let originalText = translations[indexPath.row].originalText {
+                customCell.originalText.text = originalText
+                customCell.originalText.backgroundColor = UIColor.clear
+                let size = CGSize(width: 250, height: 1000)
+                let options = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
+                let estimatedFrame = NSString(string: originalText).boundingRect(with: size, options: options, attributes: [NSAttributedString.Key.font: customCell.originalText.font!], context: nil)
+                customCell.originalText.frame = CGRect(x: 0, y: 0, width: estimatedFrame.width + 16, height: estimatedFrame.height + 10)
+            }
+            
+            if let translatedText = translations[indexPath.row].translatedText {
+                customCell.translatedText.text = translatedText
+                customCell.translatedText.backgroundColor = UIColor.clear
+                
+                let size = CGSize(width: 250, height: 1000)
+                
+                let options = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
+                let estimatedFrame = NSString(string: translatedText).boundingRect(with: size, options: options, attributes: [NSAttributedString.Key.font: customCell.translatedText.font!], context: nil)
+                customCell.translatedText.frame = CGRect(x: 0, y: customCell.originalText.frame.height - 5, width: estimatedFrame.width + 16, height: estimatedFrame.height + 10)
+            }
+            
+            if translations[indexPath.row].languageFrom == "en" {
+                let width = max(customCell.translatedText.frame.width, customCell.originalText.frame.width) + 10
+                let height = customCell.translatedText.frame.height + customCell.originalText.frame.height + 10
+                customCell.bubble.frame = CGRect(x: 0, y: 0, width: width, height: height)
+                customCell.bubble.backgroundColor = UIColor.init(red: 0, green: 124/255, blue: 233/255, alpha: 1)
+            } else {
+                let width = max(customCell.translatedText.frame.width, customCell.originalText.frame.width) + 10
+                let height = customCell.translatedText.frame.height + customCell.originalText.frame.height + 10
+                customCell.bubble.frame = CGRect(x: chatView.frame.width - width, y: 0, width: width, height: height)
+                customCell.bubble.backgroundColor = UIColor.init(red: 237/255, green: 76/255, blue: 92/255, alpha: 1)
+            }
             
             return customCell
         }
 
         return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        if let originalText = translations[indexPath.row].originalText, let translatedText = translations[indexPath.row].translatedText {
+            let originalSize = CGSize(width: 250, height: 1000)
+            let originalOptions = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
+            let originalEstimatedFrame = NSString(string: originalText).boundingRect(with: originalSize, options: originalOptions, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14)], context: nil)
+            
+            let translatedSize = CGSize(width: 250, height: 1000)
+            let translatedOptions = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
+            let translatedEstimatedFrame = NSString(string: translatedText).boundingRect(with: translatedSize, options: translatedOptions, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18)], context: nil)
+            
+            return CGSize(width: chatView.frame.width, height: originalEstimatedFrame.height + translatedEstimatedFrame.height + 30)
+        }
+        
+
+        
+        return CGSize(width: view.frame.width - 10, height: 100)
+
+    }
+    
     
     
 }
